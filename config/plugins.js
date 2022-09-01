eddf66f8a8438495903501e08f6787e83c5045fc
Pawel Dziarmaga <dziarmaga8@gmail.com>
2022-09-01 18:31:00 +0200

    Fix graphql, add readme

--- config/plugins.js
+++ config/plugins.js
@@ -15,4 +15,15 @@ module.exports = ({ env }) => ({
     },
   },
   // ...
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    introspection: true,
+    apolloServer: {
+      tracing: false,
+    },
+  },
 });
